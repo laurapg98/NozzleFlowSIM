d77932845d3845a2332d7b6e302ddf9c56861599
vilamarcc <vilamarcc@gmail.com>
2019-12-01 13:54:21 +0100

    Color Scale + combobox

--- NozzleDisplay/MainWindow.xaml.cs
+++ NozzleDisplay/MainWindow.xaml.cs
@@ -43,14 +43,13 @@ namespace NozzleDisplay
 
         public void fillCanvasNozzle()
         {
-
             for (int i = 0; i < this.nozzle.GetNumRects(); i++)
             {
                 Rectangulo rect_nozzle = this.nozzle.GetRectangulo(i);
                 Rectangle rect_canvas = new Rectangle();
                 rect_canvas.Height = rect_nozzle.GetAltura()*100;
                 rect_canvas.Width = canvasNozzle.Width / this.nozzle.GetNumRects();
-                rect_canvas.Fill = new SolidColorBrush(Colors.Red);
+                rect_canvas.Fill = new SolidColorBrush(Colors.White);
                 rect_canvas.StrokeThickness = 0;
                 rect_canvas.Stroke = Brushes.Black;
                 canvasNozzle.Children.Add(rect_canvas);
@@ -60,17 +59,60 @@ namespace NozzleDisplay
             }
         }
 
-        public Color GetColor(double rangeStart /*Complete Red*/, double rangeEnd /*Complete Green*/, double actualValue)
+        public Color GetColorMach(double rangeStart, double rangeEnd, double actualValue)
         {
             if (rangeStart >= rangeEnd) return Colors.Black;
 
             double max = rangeEnd - rangeStart; // make the scale start from 0
             double value = actualValue - rangeStart; // adjust the value accordingly
 
-            double green = (255 * value) / max; // calculate green (the closer the value is to max, the greener it gets)
-            double red = 255 - green; // set red as inverse of green
+            double red = (255 * value) / max; // calculate green (the closer the value is to max, the greener it gets)
+            double blue = 255 - red; // set red as inverse of green
 
-            return Color.FromRgb((Byte)red, (Byte)green, (Byte)0);
+            return Color.FromRgb((Byte)red, (Byte)0, (Byte)blue);
+        }
+
+        public Color GetColorTemp(double rangeStart, double rangeEnd, double actualValue)
+        {
+
+            if (actualValue >= rangeEnd) return Color.FromRgb((Byte)255, (Byte)0, (Byte)0);
+
+            double max = rangeEnd - rangeStart; // make the scale start from 0
+            double value = actualValue - rangeStart; // adjust the value accordingly
+
+            double red = (255 * value) / max; 
+            double blue = 255 - red;
+            double green = blue / 2;
+
+            return Color.FromRgb((Byte)red, (Byte)green, (Byte)blue);
+        }
+
+        public Color GetColorPressure(double rangeStart, double rangeEnd, double actualValue)
+        {
+            if (rangeStart >= rangeEnd) return Colors.Black;
+
+            double max = rangeEnd - rangeStart; // make the scale start from 0
+            double value = actualValue - rangeStart; // adjust the value accordingly
+
+            double blue = (255 * value) / max; // calculate green (the closer the value is to max, the greener it gets)
+            double red = 255 - blue; // set red as inverse of green
+            double green = blue / 2;
+
+            return Color.FromRgb((Byte)red, (Byte)green, (Byte)blue);
+        }
+
+        public Color GetColorDensity(double rangeStart, double rangeEnd, double actualValue)
+        {
+            if (actualValue >= rangeEnd) return Colors.Black;
+
+            double max = rangeEnd - rangeStart; // make the scale start from 0
+            double value = actualValue - rangeStart; // adjust the value accordingly
+
+            double blue = 255 - ((255 * value) / max); 
+            double red = Math.Min(255,blue*2*value);
+            double green = 0;
+
+            return Color.FromRgb((Byte)red, (Byte)green, (Byte)blue);
         }
 
         private void parambut_Click(object sender, RoutedEventArgs e)
@@ -81,9 +123,87 @@ namespace NozzleDisplay
             fillCanvasNozzle();
         }
 
-        public void colorRectangleMach(Rectangle rect)
+        private void comboboxcolor_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            try
+            {
+                refreshCanvas();
+            }
+            catch
+            {
 
+            }
+        }
+
+        private void refreshCanvas()
+        {
+            if (comboboxcolor.SelectedIndex == 0) //Pressure
+            {
+                for (int i = 0; i < nozzlerectangles.Length; i++)
+                {
+                    Rectangle rect_canvas = this.nozzlerectangles[i];
+                    Rectangulo rect_nozzle = this.nozzle.GetRectangulo(i);
+                    rect_canvas.Fill = new SolidColorBrush(GetColorPressure(0, 1, rect_nozzle.GetPresP()));
+                }
+
+                LinearGradientBrush lgb = new LinearGradientBrush(GetColorPressure(0, 1, 1), GetColorPressure(0, 1, 0), 90);
+                GradientStop gs = new GradientStop();
+                gs.Color = GetColorPressure(0, 1, 0.5);
+                gs.Offset = 0.5;
+                lgb.GradientStops.Add(gs);
+                rectanglescale.Fill = lgb;
+            }
+
+            if (comboboxcolor.SelectedIndex == 1) //Mach
+            {
+                for (int i = 0; i < nozzlerectangles.Length; i++)
+                {
+                    Rectangle rect_canvas = this.nozzlerectangles[i];
+                    Rectangulo rect_nozzle = this.nozzle.GetRectangulo(i);
+                    rect_canvas.Fill = new SolidColorBrush(GetColorMach(0, 4, rect_nozzle.GetVelP()));
+                }
+
+                LinearGradientBrush lgb = new LinearGradientBrush(GetColorMach(0, 4, 4), GetColorMach(0, 4, 0), 90);
+                GradientStop gs = new GradientStop();
+                gs.Color = GetColorMach(0, 4, 2);
+                gs.Offset = 0.5;
+                lgb.GradientStops.Add(gs);
+                rectanglescale.Fill = lgb;
+            }
+
+            if (comboboxcolor.SelectedIndex == 2) //Temperature
+            {
+                for (int i = 0; i < nozzlerectangles.Length; i++)
+                {
+                    Rectangle rect_canvas = this.nozzlerectangles[i];
+                    Rectangulo rect_nozzle = this.nozzle.GetRectangulo(i);
+                    rect_canvas.Fill = new SolidColorBrush(GetColorTemp(0, 1, rect_nozzle.GetTempP()));
+                }
+
+                LinearGradientBrush lgb = new LinearGradientBrush(GetColorTemp(0, 1, 1), GetColorTemp(0, 1, 0), 90);
+                GradientStop gs = new GradientStop();
+                gs.Color = GetColorTemp(0, 1, 0.5);
+                gs.Offset = 0.5;
+                lgb.GradientStops.Add(gs);
+                rectanglescale.Fill = lgb;
+            }
+
+            if (comboboxcolor.SelectedIndex == 3) //Density
+            {
+                for (int i = 0; i < nozzlerectangles.Length; i++)
+                {
+                    Rectangle rect_canvas = this.nozzlerectangles[i];
+                    Rectangulo rect_nozzle = this.nozzle.GetRectangulo(i);
+                    rect_canvas.Fill = new SolidColorBrush(GetColorDensity(0, 1, rect_nozzle.GetDensP()));
+                }
+
+                LinearGradientBrush lgb = new LinearGradientBrush(GetColorDensity(0, 1, 1), GetColorDensity(0, 1, 0), 90);
+                GradientStop gs = new GradientStop();
+                gs.Color = GetColorDensity(0, 1, 0.5);
+                gs.Offset = 0.5;
+                lgb.GradientStops.Add(gs);
+                rectanglescale.Fill = lgb;
+            }
         }
     }
 }
